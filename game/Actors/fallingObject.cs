5d81e9b902bc149da1909bf08e166209ddcd622d
jalliaud <juampialliaud@gmail.com>
2022-11-01 15:25:38 -0600

    modified:   game/Actors/fallingObject.cs

--- game/Actors/fallingObject.cs
+++ game/Actors/fallingObject.cs
@@ -4,9 +4,35 @@ using System.Collections.Generic;
 
 namespace Lab04.Game
 {
-    public class fallingObject
+    public class fallingObject : Actor
     
     {
+         private string _message = "";
+
+        /// <summary>
+        /// Constructs a new instance of an Artifact.
+        /// </summary>
+        public fallingObject()
+        {
+        }
+
+        /// <summary>
+        /// Gets the artifact's message.
+        /// </summary>
+        /// <returns>The message.</returns>
+        public string GetMessage()
+        {
+            return _message;
+        }
+
+        /// <summary>
+        /// Sets the artifact's message to the given value.
+        /// </summary>
+        /// <param name="message">The given message.</param>
+        public void SetMessage(string message)
+        {
+            this._message = message;
+        }
 
     }
 }
