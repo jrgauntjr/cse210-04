4c94c81b2f0484c4ed44f17462a332078bc7d059
Emma-John <joh18039@byui.edu>
2022-11-01 15:34:40 -0600

    copied RFK

--- game/Director/Director.cs
+++ game/Director/Director.cs
@@ -1,37 +1,96 @@
-using System;
 using System.Collections.Generic;
+using Unit04.Game.Casting;
+using Unit04.Game.Services;
 
-namespace Lab04.Game
+
+namespace Unit04.Game.Directing
 {
+    /// <summary>
+    /// <para>A person who directs the game.</para>
+    /// <para>
+    /// The responsibility of a Director is to control the sequence of play.
+    /// </para>
+    /// </summary>
     public class Director
     {
-        public Director()
+        private KeyboardService _keyboardService = null;
+        private VideoService _videoService = null;
+
+        /// <summary>
+        /// Constructs a new instance of Director using the given KeyboardService and VideoService.
+        /// </summary>
+        /// <param name="keyboardService">The given KeyboardService.</param>
+        /// <param name="videoService">The given VideoService.</param>
+        public Director(KeyboardService keyboardService, VideoService videoService)
         {
+            this._keyboardService = keyboardService;
+            this._videoService = videoService;
         }
-        bool isPlaying = true;
-        public void StartGame()
+
+        /// <summary>
+        /// Starts the game by running the main game loop for the given cast.
+        /// </summary>
+        /// <param name="cast">The given cast.</param>
+        public void StartGame(Cast cast)
         {
-            while (isPlaying == true)
+            _videoService.OpenWindow();
+            while (_videoService.IsWindowOpen())
             {
-                getInputs();
-                doUpdates();
-                doOutputs();
+                GetInputs(cast);
+                DoUpdates(cast);
+                DoOutputs(cast);
             }
-        }
-        public void getInputs()
-        {
-            // getKeyboard();
+            _videoService.CloseWindow();
         }
 
-        public void doUpdates()
+        /// <summary>
+        /// Gets directional input from the keyboard and applies it to the robot.
+        /// </summary>
+        /// <param name="cast">The given cast.</param>
+        private void GetInputs(Cast cast)
         {
-            //fallingObject.move;
-            //player.move;
+            Actor robot = cast.GetFirstActor("robot");
+            Point velocity = _keyboardService.GetDirection();
+            robot.SetVelocity(velocity);     
         }
 
-        public void doOutputs()
+        /// <summary>
+        /// Updates the robot's position and resolves any collisions with artifacts.
+        /// </summary>
+        /// <param name="cast">The given cast.</param>
+        private void DoUpdates(Cast cast)
         {
-            //getVideoService;
+            Actor banner = cast.GetFirstActor("banner");
+            Actor robot = cast.GetFirstActor("robot");
+            List<Actor> artifacts = cast.GetActors("artifacts");
+
+            banner.SetText("");
+            int maxX = _videoService.GetWidth();
+            int maxY = _videoService.GetHeight();
+            robot.MoveNext(maxX, maxY);
+
+            foreach (Actor actor in artifacts)
+            {
+                if (robot.GetPosition().Equals(actor.GetPosition()))
+                {
+                    Artifact artifact = (Artifact) actor;
+                    string message = artifact.GetMessage();
+                    banner.SetText(message);
+                }
+            } 
         }
+
+        /// <summary>
+        /// Draws the actors on the screen.
+        /// </summary>
+        /// <param name="cast">The given cast.</param>
+        public void DoOutputs(Cast cast)
+        {
+            List<Actor> actors = cast.GetAllActors();
+            _videoService.ClearBuffer();
+            _videoService.DrawActors(actors);
+            _videoService.FlushBuffer();
+        }
+
     }
 }
